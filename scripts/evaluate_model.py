d368bff5e2b77696f627ed2f52bfa48887c1b2a9
pkseniya <petrushina.ke@phystech.edu>
2022-12-06 13:29:53 +0300

    Revert "add model and training"
    This reverts commit 50e16a5aa7594f6b578872a10dba98368571e1b6.

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -1,28 +1,11 @@
-import argparse
-
 from torch.utils.data import DataLoader
 from transformers import AutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
 
-from src.data.dataset import ConvAI2Dataset
-from src.metrics import calculate_f1
-from src.metrics import calculate_hits_ppl
-
-
-def configure_arg_parser():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-m", "--diffusion", help="Seq2seq diffusion name from huggingface")
-    parser.add_argument("-p", "--path", help="Path to dataset for evaluation")
-    parser.add_argument("-bs", "--batch_size", type=int, default=64, help="Batch size for evaluation")
-    parser.add_argument("-s", "--side", default="left", help="Side of truncation and padding for tokenizer")
-    parser.add_argument("-cl", "--max_context", type=int, default=256, help="Max length of context fed into diffusion")
-    parser.add_argument("-gl", "--max_gen", type=int, default=64, help="Max length of generated output")
-    parser.add_argument("-d", "--device", default="cpu", help="Device on which to evaluate the diffusion")
-    parser.add_argument("-ds", "--do_sample", default=True, help="parameter for generation method")
-    parser.add_argument("-nb", "--num_beams", default=4, help="Parameter for generation method")
-
-    return parser
+from evaluation_utils import ConvAI2Dataset
+from evaluation_utils import calculate_f1
+from evaluation_utils import calculate_hits_ppl
+from evaluation_utils import configure_arg_parser
 
 
 def main(model, path, batch_size, side, max_context, max_gen, device, do_sample, num_beams):
