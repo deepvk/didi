3fdf840cfb3957f673d522091f44557f9f0a062e
Egor Spirin <egor.spirin@vk.team>
2023-01-27 20:38:18 +0400

    Specify context and target seq lengths for dataset, return only true candidate in collate fn

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -5,7 +5,6 @@ from transformers import AutoTokenizer
 
 from src.data.dataset import ConvAI2Dataset
 from src.diffusion.model import Seq2SeqDiffusionTransformer
-from src.diffusion.train_utils import collate_gt
 from src.diffusion.train_utils import train_model
 
 
@@ -16,7 +15,6 @@ def configure_arg_parser():
     parser.add_argument("-t", "--train", help="Path to dataset for training")
     parser.add_argument("-v", "--val", help="Path to dataset for validation")
     parser.add_argument("-bs", "--batch_size", type=int, default=64, help="Batch size for training")
-    parser.add_argument("-s", "--side", default="right", help="Side of truncation and padding for tokenizer")
     parser.add_argument("-cl", "--max_context", type=int, default=256, help="Max length of context fed into diffusion")
     parser.add_argument("-gl", "--max_gen", type=int, default=64, help="Max length of generated output")
     parser.add_argument("-d", "--device", default="cpu", help="Device on which to evaluate the diffusion")
@@ -27,17 +25,17 @@ def configure_arg_parser():
     return parser
 
 
-def main(model, train, val, batch_size, side, max_context, max_gen, device, schedule, diffusion_steps, train_steps):
-    tokenizer = AutoTokenizer.from_pretrained(model, truncation_side=side, padding_side=side)
+def main(model, train, val, batch_size, max_context, max_gen, device, schedule, diffusion_steps, train_steps):
+    tokenizer = AutoTokenizer.from_pretrained(model, truncation_side="left")
 
-    train_dataset = ConvAI2Dataset(train, tokenizer)
+    train_dataset = ConvAI2Dataset(train, tokenizer, max_context, max_gen)
     train_dataloader = DataLoader(
-        train_dataset, batch_size=batch_size, collate_fn=lambda x: collate_gt(x, tokenizer, max_context, max_gen)
+        train_dataset, batch_size=batch_size, collate_fn=lambda x: train_dataset.collate_fn(x, False)
     )
 
-    val_dataset = ConvAI2Dataset(val)
+    val_dataset = ConvAI2Dataset(val, tokenizer, max_context, max_gen)
     val_dataloader = DataLoader(
-        val_dataset, batch_size=batch_size, collate_fn=lambda x: collate_gt(x, tokenizer, max_context, max_gen)
+        val_dataset, batch_size=batch_size, collate_fn=lambda x: val_dataset.collate_fn(x, False)
     )
 
     model = Seq2SeqDiffusionTransformer(model, tokenizer.vocab_size, diffusion_steps).to(device)
