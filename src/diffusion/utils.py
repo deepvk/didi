eef17567d06b2fd286740328bfba9a5494393b26
pkseniya <petrushina.ke@phystech.edu>
2023-03-06 15:06:40 +0300

    fix mypy

--- src/diffusion/utils.py
+++ src/diffusion/utils.py
@@ -24,7 +24,7 @@ def configure_schedule(steps: int, schedule: str):
     alphas_cumprod = torch.cumprod(alphas, dim=0)
     alphas_cumprod_prev = F.pad(alphas_cumprod, (1, 0), value=1.0)
 
-    sigma_0 = torch.sqrt(betas[0])
+    sigma_0 = torch.sqrt(betas[0]).item()
 
     return alphas_cumprod_prev, sigma_0
 
@@ -38,7 +38,7 @@ def get_xt(x_0, alphas_cumprod_prev, t):
     return x_t
 
 
-def prepare_x0(emb: torch.Tensor, sigma_0: torch.Tensor):
+def prepare_x0(emb: torch.Tensor, sigma_0: float):
     noise = torch.normal(0, sigma_0, size=emb.shape).to(emb.device)
     x_0 = emb + noise
 
@@ -49,7 +49,7 @@ def get_diffusion_variables(
     diffusion_steps: int,
     emb: torch.Tensor,
     alphas_cumprod_prev: torch.Tensor,
-    sigma_0: torch.Tensor,
+    sigma_0: float,
 ):
     x_0 = prepare_x0(emb, sigma_0)
 
