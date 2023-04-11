6c5e2a3c5f52a4c8dceb4efbcc9d45a2d8e0bcb3
Kseniia Petrushina <56446605+pkseniya@users.noreply.github.com>
2023-04-11 13:15:04 +0300

    Euler sampling (#9)
    * sigma model
    
    * add euler sampling
    
    * update xT
    
    * complete euler sampling + back to timesteps
    
    * fixes
    
    * move noise + types
    
    * fix cached context
    
    * return emb->x0

--- src/diffusion/model.py
+++ src/diffusion/model.py
@@ -4,7 +4,7 @@ from torch import nn
 from transformers import AutoModel
 from transformers import BertConfig
 
-from src.diffusion.utils import configure_schedule, get_diffusion_variables, prepare_x0, get_xt
+from src.diffusion.utils import configure_schedule, get_x0, scale_input, get_diffusion_variables, get_euler_variables
 from src.metrics import calculate_batch_ce
 
 
@@ -60,6 +60,9 @@ class DiDi(LightningModule):
         optimizer: str = "AdamW",
         lr: float = 0.0001,
         momentum: float = 0.95,
+        s_churn: float = 0.0,
+        s_tmin: float = 0.0,
+        s_tmax: float = float("+inf"),
     ):
         super().__init__()
         self.diffusion_steps = diffusion_steps
@@ -69,14 +72,19 @@ class DiDi(LightningModule):
         self.emb = nn.Embedding(vocabulary_size, emb_dim, padding_idx=pad_idx)
         self.time_embeds = nn.Embedding(diffusion_steps + 1, emb_dim)
 
+        self.s_churn = s_churn
+        self.s_tmin = s_tmin
+        self.s_tmax = s_tmax
+
         self.encoder = encoder
         freeze_params(self.encoder)
 
         self.decoder = decoder
         self.classifier = nn.Linear(emb_dim, vocabulary_size)
 
-        alphas_cumprod_prev, self.sigma_0 = configure_schedule(diffusion_steps, schedule)
-        self.register_buffer("alphas_cumprod_prev", alphas_cumprod_prev)
+        sigmas, std_0 = configure_schedule(diffusion_steps, schedule)
+        self.register_buffer("sigmas", sigmas)
+        self.register_buffer("std_0", std_0)
 
         self.optimizer = optimizer
         self.lr = lr
@@ -101,11 +109,11 @@ class DiDi(LightningModule):
 
     def forward(
         self,
-        encoder_input_ids=None,
-        encoder_attention_mask=None,
-        decoder_inputs_embeds=None,
-        time_ids=None,
-        context=None,
+        encoder_input_ids: torch.Tensor = None,
+        encoder_attention_mask: torch.Tensor = None,
+        decoder_inputs_embeds: torch.Tensor = None,
+        time_ids: torch.Tensor = None,
+        context: torch.Tensor = None,
     ):
         if encoder_input_ids is None and context is None:
             raise ValueError("Either `encoder_input_ids` or `context` must be provided.")
@@ -117,22 +125,27 @@ class DiDi(LightningModule):
                 ).last_hidden_state
 
         time_embeds = self.time_embeds(time_ids)
-        input_embeds = decoder_inputs_embeds + time_embeds.unsqueeze(1)
+        input_embeds = decoder_inputs_embeds + time_embeds
 
         output = self.decoder(
-            inputs_embeds=input_embeds, encoder_hidden_states=context, encoder_attention_mask=encoder_attention_mask
+            inputs_embeds=input_embeds,
+            encoder_hidden_states=context,
+            encoder_attention_mask=encoder_attention_mask,
         ).last_hidden_state
         return output, context
 
-    def training_step(self, batch, batch_idx):
-        context, target = batch
+    def training_step(self, batch: list, batch_idx: int):
+        raw_context, target = batch
         emb = self.emb(target.input_ids)
+        x_0 = get_x0(emb, self.std_0)
+        noise = torch.randn_like(x_0)
 
         # x: [batch size; seq len; emb dim], t: [batch size]
-        x_0, x_t, t = get_diffusion_variables(self.diffusion_steps, emb, self.alphas_cumprod_prev, self.sigma_0)
+        x_t, t = get_diffusion_variables(self.diffusion_steps, x_0, self.sigmas, noise)
+
         x_0_hat, _ = self(
-            encoder_input_ids=context.input_ids,
-            encoder_attention_mask=context.attention_mask,
+            encoder_input_ids=raw_context.input_ids,
+            encoder_attention_mask=raw_context.attention_mask,
             decoder_inputs_embeds=x_t,
             time_ids=t,
         )  # [batch size; seq len; emb dim]
@@ -146,6 +159,7 @@ class DiDi(LightningModule):
             flat_mean((x_0_hat - emb) ** 2 * non_pad_mask),
             flat_mean((x_0_hat - x_0) ** 2 * non_pad_mask),
         ).mean()
+
         t0_loss = (x_0**2 * non_pad_mask).mean()
         loss = mse + ce + t0_loss
 
@@ -153,35 +167,43 @@ class DiDi(LightningModule):
         self.log_dict(metrics, sync_dist=True, on_step=True, on_epoch=False)
         return loss
 
-    def validation_step(self, batch, batch_idx):
-        context, target = batch
+    def euler_step(self, x_t, sigma_t, raw_context, cached_context, t, next_t, num_sigmas, ones, noise):
+        x_t = scale_input(x_t, sigma_t)
+
+        x_0, cached_context = self(
+            encoder_input_ids=raw_context.input_ids,
+            encoder_attention_mask=raw_context.attention_mask,
+            decoder_inputs_embeds=x_t,
+            time_ids=t * ones,
+            context=cached_context,
+        )
+
+        x_t, sigma_hat = get_euler_variables(x_t, noise, sigma_t, self.s_churn, self.s_tmin, self.s_tmax, num_sigmas)
+
+        d = (x_t - x_0) / sigma_t
+        dt = self.sigmas[next_t] - sigma_hat
+        x_t = x_t + d * dt
+        return x_t, cached_context
+
+    def validation_step(self, batch: list, batch_idx: int):
+        raw_context, target = batch
         emb = self.emb(target.input_ids)
-        x_0 = prepare_x0(emb, self.sigma_0)
+        x_t = torch.randn_like(emb) * self.sigmas[-1]
 
         cached_context = None
-        ones = torch.ones(x_0.shape[0], dtype=torch.long, device=x_0.device)
-        noise = torch.empty_like(x_0)
-        for time in range(self.diffusion_steps, 1, -self.step_freq):
-            t = ones * time
+        ones = torch.ones((emb.shape[0], 1), dtype=torch.long, device=emb.device)
+        noise = torch.empty_like(emb)
+
+        num_sigmas = len(range(self.diffusion_steps, 1, -self.step_freq))
+
+        for t in range(self.diffusion_steps, 1, -self.step_freq):
             noise.normal_(0, 1)
-            x_t = get_xt(x_0, self.alphas_cumprod_prev, t, noise)
-            x_0, cached_context = self(
-                encoder_input_ids=context.input_ids,
-                encoder_attention_mask=context.attention_mask,
-                decoder_inputs_embeds=x_t,
-                time_ids=t,
-                context=cached_context,
+            x_t, cached_context = self.euler_step(
+                x_t, self.sigmas[t], raw_context, cached_context, t, max(t - self.step_freq, 1), num_sigmas, ones, noise
             )
 
         noise.normal_(0, 1)
-        x_1 = get_xt(x_0, self.alphas_cumprod_prev, ones, noise)
-        x_0, _ = self(
-            encoder_input_ids=context.input_ids,
-            encoder_attention_mask=context.attention_mask,
-            decoder_inputs_embeds=x_1,
-            time_ids=ones,
-            context=cached_context,
-        )
+        x_0, _ = self.euler_step(x_t, self.sigmas[1], raw_context, cached_context, 1, 0, num_sigmas, ones, noise)
 
         logits = self.classifier(x_0)
         predictions = logits.argmax(-1)
--- src/diffusion/utils.py
+++ src/diffusion/utils.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn.functional as F
 
 from src.diffusion.schedules import cosine_beta_schedule, linear_beta_schedule, sqrt_beta_schedule
 
@@ -16,30 +15,41 @@ def configure_schedule(steps: int, schedule: str):
 
     alphas = 1 - betas
     alphas_cumprod = torch.cumprod(alphas, dim=0)
-    alphas_cumprod_prev = F.pad(alphas_cumprod, (1, 0), value=1.0)
+    sigmas = ((1 - alphas_cumprod) / alphas_cumprod) ** 0.5
+    sigmas = torch.cat([sigmas.new_zeros([1]), sigmas])
 
-    sigma_0 = torch.sqrt(betas[0]).item()
-
-    return alphas_cumprod_prev, sigma_0
-
-
-def get_xt(x_0, alphas_cumprod_prev, t, noise=None):
-    if noise is None:
-        noise = torch.normal(0, 1, size=x_0.shape, device=x_0.device)
-
-    alphas_cumprod_prev_t = alphas_cumprod_prev[t].view(-1, 1, 1)
-    x_t = torch.sqrt(alphas_cumprod_prev_t) * x_0 + torch.sqrt(1 - alphas_cumprod_prev_t) * noise
-    return x_t
+    return sigmas, torch.sqrt(betas[0])
 
 
-def prepare_x0(emb: torch.Tensor, sigma_0: float):
-    noise = torch.normal(0, sigma_0, size=emb.shape, device=emb.device)
-    x_0 = emb + noise
+def get_x0(emb, std):
+    noise = torch.randn_like(emb)
+    x_0 = emb + std * noise
     return x_0
 
 
-def get_diffusion_variables(diffusion_steps: int, emb: torch.Tensor, alphas_cumprod_prev: torch.Tensor, sigma_0: float):
-    x_0 = prepare_x0(emb, sigma_0)
-    t = torch.randint(1, diffusion_steps + 1, size=(x_0.shape[0],), device=x_0.device)
-    x_t = get_xt(x_0, alphas_cumprod_prev, t)
-    return x_0, x_t, t
+def scale_input(input, sigma_t):
+    return input / (sigma_t**2 + 1) ** 0.5
+
+
+def get_diffusion_variables(diffusion_steps: int, x_0: torch.Tensor, sigmas: torch.Tensor, noise: torch.Tensor):
+    t = torch.randint(1, diffusion_steps + 1, size=(x_0.shape[0], 1), device=x_0.device)
+    sigma_t = sigmas[t].view(-1, 1, 1)
+    x_t = scale_input(x_0 + sigma_t * noise, sigma_t)
+    return x_t, t
+
+
+def get_euler_variables(
+    x_t: torch.Tensor,
+    noise: torch.Tensor,
+    sigma_t: torch.Tensor,
+    s_churn: float,
+    s_tmin: float,
+    s_tmax: float,
+    num_sigmas: int,
+):
+    gamma = min(s_churn / num_sigmas, 2**0.5 - 1) if s_tmin <= sigma_t <= s_tmax else 0.0
+    sigma_hat = sigma_t * (gamma + 1)
+
+    if gamma > 0:
+        x_t = x_t + (sigma_hat**2 - sigma_t**2) ** 0.5 * noise
+    return x_t, sigma_hat
