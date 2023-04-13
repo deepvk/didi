f87a8d6566fe8987e09cb5ac9d9690442441cbbf
Egor Spirin <egor.spirin@vk.team>
2023-04-13 20:57:45 +0300

    Use custom ckpt directory

--- scripts/reddit_pretrain.py
+++ scripts/reddit_pretrain.py
@@ -1,10 +1,12 @@
 import argparse
+from os import environ
 from os.path import join
 
 import torch
 from omegaconf import OmegaConf
 from torch.utils.data import DataLoader
 
+from src.data.distributed_dataset import DistributedIterableDataset
 from src.data.reddit_dataset import RedditDataset
 from src.diffusion.model import DiDi
 from src.diffusion.model import get_components
@@ -16,12 +18,15 @@ def configure_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("config_path", type=str, help="Path to YAML config file")
     parser.add_argument("dataset_dir", type=str, help="Path to dataset directory")
+    parser.add_argument("--ckpt_dir", type=str, help="Path to checkpoint directory.")
     return parser
 
 
-def main(config_path: str, dataset_dir: str):
+def main(config_path: str, dataset_dir: str, ckpt_dir: str = None):
     filter_warnings()
     setup_logger()
+    environ["TOKENIZERS_PARALLELISM"] = "false"
+
     torch.set_float32_matmul_precision("high")
 
     config = OmegaConf.load(config_path)
@@ -30,20 +35,34 @@ def main(config_path: str, dataset_dir: str):
     train_files_glob = join(dataset_dir, "train", "train.jsonl-*")
     train_dataset = RedditDataset(train_files_glob, config.base_name, infinite=True, **config.dataset)
     train_dataloader = DataLoader(
-        train_dataset, batch_size=config.batch_size, collate_fn=train_dataset.collate_fn, pin_memory=True
+        DistributedIterableDataset(train_dataset),
+        batch_size=config.batch_size,
+        collate_fn=train_dataset.collate_fn,
+        pin_memory=True,
+        num_workers=1,
     )
 
-    val_files_glob = join(dataset_dir, "val", "val.jsonl-*")
+    val_files_glob = join(dataset_dir, "val.jsonl")
     val_dataset = RedditDataset(val_files_glob, config.base_name, infinite=False, **config.dataset)
     val_dataloader = DataLoader(
-        val_dataset, batch_size=config.val_batch_size, collate_fn=val_dataset.collate_fn, pin_memory=True
+        DistributedIterableDataset(val_dataset),
+        batch_size=config.val_batch_size,
+        collate_fn=val_dataset.collate_fn,
+        pin_memory=True,
+        num_workers=1,
     )
 
     encoder, decoder, emb_dim = get_components(config.base_name, **config.decoder)
     model = DiDi(encoder, decoder, emb_dim, train_dataset.vocab_size, pad_idx=train_dataset.pad_idx, **config.didi)
 
     train_model(
-        model, train_dataloader, val_dataloader, config.trainer, seed=config.seed, save_interval=config.save_interval
+        model,
+        train_dataloader,
+        val_dataloader,
+        config.trainer,
+        seed=config.seed,
+        save_interval=config.save_interval,
+        ckpt_dir=ckpt_dir,
     )
 
 
--- src/training.py
+++ src/training.py
@@ -1,6 +1,9 @@
+from os.path import join
+
 from lightning import seed_everything, Trainer
 from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor
 from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.strategies import DDPStrategy
 
 
 def train_model(
@@ -12,12 +15,16 @@ def train_model(
     seed: int = 42,
     save_interval: int = -1,
     project_name: str = "didi",
+    ckpt_dir: str = None
 ):
     seed_everything(seed)
 
     wandb_logger = WandbLogger(project=project_name)
+    wandb_logger.log_hyperparams(train_parameters)
+
+    ckpt_dir = join(ckpt_dir, str(wandb_logger.experiment.id)) if ckpt_dir is not None else wandb_logger.experiment.dir
     checkpoint_callback = ModelCheckpoint(
-        str(wandb_logger.experiment.dir),
+        str(ckpt_dir),
         filename="step_{step}",
         every_n_train_steps=save_interval if save_interval > 0 else train_parameters["max_steps"],
         save_top_k=-1,
@@ -27,7 +34,10 @@ def train_model(
 
     lr_logger = LearningRateMonitor("step")
 
+    strategy = DDPStrategy(find_unused_parameters=False, static_graph=True, gradient_as_bucket_view=True)
     trainer = Trainer(
+        accelerator="gpu",
+        strategy=strategy,
         callbacks=[lr_logger, checkpoint_callback],
         logger=wandb_logger,
         log_every_n_steps=10,
